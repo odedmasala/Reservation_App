1a319128b5cee1040f34c65222f002f1cfd7a2c4
odedmasala <odedmasala6@gmail.com>
2022-10-05 21:17:30 +0300

    update

--- api/index.js
+++ api/index.js
@@ -1,26 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { autRoute, hotelsRoute, usersRoute, roomsRoute } from "./routes/index.js";
 const app = express();
 dotenv.config();
-
 
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
     console.log("The DB login was successful");
   } catch (error) {
-    throw error 
+    throw error;
   }
 };
 
-mongoose.connection.on("disconnected",()=>{
-    console.log("mongo DB disconnected");
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("mongo DB disconnected");
+});
 
-
+app.use("api/auth", autRoute);
+app.use("api/users", usersRoute);
+app.use("api/hotels", hotelsRoute);
+app.use("api/room", roomsRoute);
 
 app.listen(8000, () => {
-  connect()
-  console.log("connect to backend");
+  connect();
+  console.log("connect to backend at URL http://localhost:8000/api");
 });
---
+++ api/routes/index.js
@@ -0,0 +1,6 @@
+import autRoute from "./auth.js"
+import hotelsRoute from "./hotels.js"
+import roomsRoute from "./rooms.js"
+import usersRoute from "./users.js"
+
+export{autRoute,roomsRoute,usersRoute,hotelsRoute}
