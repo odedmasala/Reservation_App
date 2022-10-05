c13d14580bfb2ec82ab3267b81625bc6c702b300
odedmasala <odedmasala6@gmail.com>
2022-10-05 21:04:43 +0300

    start routes

--- api/index.js
+++ api/index.js
@@ -2,14 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 const app = express();
-dotenv.config()
+dotenv.config();
 
-try {
+
+const connect = async () => {
+  try {
     await mongoose.connect(process.env.MONGO);
+    console.log("The DB login was successful");
   } catch (error) {
-    handleError(error);
+    throw error 
   }
+};
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongo DB disconnected");
+})
+
+
 
 app.listen(8000, () => {
+  connect()
   console.log("connect to backend");
 });
