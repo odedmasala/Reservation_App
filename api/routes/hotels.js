cbe904ef78a599728dae8ec78ad1cc3d0525f24e
odedmasala <odedmasala6@gmail.com>
2022-10-17 14:01:11 +0300

    update

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,17 +1,28 @@
 import express from "express";
-import { createHotel, deleteHotel, getallHotels, getHotel, updateHotel } from "../controllers/hotel.js";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getallHotels,
+  getHotel,
+  getHotelRooms,
+  updateHotel,
+} from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 // CREATE
-router.post("/",verifyAdmin, createHotel);
+router.post("/", verifyAdmin, createHotel);
 // UPDATE
-router.put("/:id",verifyAdmin, updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 // DELETE
-router.delete("/:id",verifyAdmin, deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 // GET
 router.get("/:id", getHotel);
 // GET ALL
 router.get("/", getallHotels);
-
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 export default router;
