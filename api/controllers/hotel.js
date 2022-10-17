1c77b23f409e86144404526241a86f24b70aa6c7
odedmasala <odedmasala6@gmail.com>
2022-10-17 17:41:14 +0300

    update

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -43,11 +43,15 @@ export const createHotel = async (req, res,next) => {
   };
 
   export const getallHotels = async (req, res, next) => {
+    const { min, max, ...others } = req.query;
     try {
-      const hotels = await Hotel.find();
+      const hotels = await Hotel.find({
+        ...others,
+        cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+      }).limit(req.query.limit);
       res.status(200).json(hotels);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
   export const countByCity = async (req, res, next) => {
--- client/src/component/navbar/Navbar.jsx
+++ client/src/component/navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 
 const Navbar = () => {
   return (
     <div className="bg-[#003580] flex justify-center h-[50px]">
       <div className="w-[100%] p-2  flex justify-between items-center max-w-5xl text-white">
+      <Link to="/" className='text-[inherit] decoration-auto'>
+
         <span className="font-medium">lamabooking</span>
+        </Link>
+
         <div className="">
           <button className="bg-white p-1 text-black ml-[20px]">Register</button>
           <button className="bg-white p-1 text-black ml-[20px]">Login</button>
